ad6b61a96f710bbe5bd8d05382ef9aaad593496a
Zebfred <zebfred22@gmail.com>
2019-12-03 15:57:54 -0800

    This should be better organized

---
+++ Lambdata_folder_eh/ex1.py
@@ -0,0 +1,39 @@
+if __name__ == "__main__":
+    example code here
+    
+from packagename.Filename import Classname
+
+folder Lambda_Zeb2
+-Class1.py
+-Class2.py
+
+
+import all methods 
+Mylib
+-__init__.py
+-File1.py
+-File2.py
+
+#Inside of __init__.py
+from MyLib.File1 import ClassA, ClassB, ClassC
+from MyLib.File2 import ClassX, ClassY, ClassZ
+
+#package folder 
+#-MyLib
+#setup.py
+download url
+install_requires
+class1 file includes:
+    import numpy as np
+    from scrapeasy import Webpage
+    import time 
+    install_requires = [
+        'numpy', 
+        'scrapeasy'
+    ]
+#setup.cfg
+Inside of setup.cfg
+[metadata]
+description-file = ReadMe.md
+#License.txt
+#ReadMe.md
